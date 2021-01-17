b4b04fdbc66cea5a0bbc9b4aa3a43f6fcb888367
Gilmer Contreras <gacpdesgins@gmail.com>
2021-01-17 00:57:23 -0400

    Inicio del Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// COMPONENTS
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
+
+const routes: Routes = [
+
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'clientes', component: ClientesComponent },
+      { path: 'ventas', component: VentasComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: NopagefoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    VentasComponent,
+    ClientesComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
